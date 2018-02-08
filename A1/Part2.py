11c17833d676be2650dcd7fcd5f0091c6a805505
Ibrahim A <iabde039@gmail.com>
2018-02-07 23:21:56 -0500

    release01

---
+++ A1/Part2.py
@@ -0,0 +1,11 @@
+print("Hamlet's Soliloquy Mad Lib!!")
+
+v1=input("Please enter a verb: ")
+a1=input("Please enter an adjective: ")
+wp=input("Please enter a weapon type(plural): ")
+v2=input("Please enter a verb: ")
+v3=input("Please enter a verb: ")
+v4=input("Please enter a verb: ")
+n1=input("Please enter a noun: ")
+
+print("To %s, or not to %s, that is the question: Whether `tis more %s in the mind to suffer the slings and %s of outrageous fortune or to take arms against a sea of troubles, and by opposing end them? To die, to %s no more; and by a sleep to say we end the heartache, and the thousand natural shocks that flesh is heir to, `tis a consummation devoutly to be wish`d. To die, to %s; To %s: perchance to dream: ay, there`s the rub; for in that sleep of death what dreams may come, when we have shuffled off this mortal %s, must give us pause: there`s the respect that makes calamity of so long life..."%(v1,v1,a1,wp,v2,v3,v4,n1))
