ae49dbde0d21ea64d868733a930104e512631dd8
Ibrahim A <iabde039@gmail.com>
2018-02-07 23:40:05 -0500

    added part1.py

---
+++ A1/Part1.py
@@ -0,0 +1,7 @@
+n1=float(input("Enter first number: "))
+n2=float(input("Enter second number: "))
+
+print("The sum of the two numbers is", n1+n2)
+print("The difference of the two numbers is", abs(n1-n2))
+print("The product of the two numbers is", n1*n2)
+print("The average of the two numbers is", (n1+n2)/2)
